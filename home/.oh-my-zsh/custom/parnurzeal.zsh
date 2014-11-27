1c2016d5aa93275918ed725191eed5e4af204d92
parnurzeal <parnurzeal@gmail.com>
2014-11-27 10:17:00 +0900

    Updated my own custom zsh and tmux conf

---
+++ home/.oh-my-zsh/custom/parnurzeal.zsh
@@ -0,0 +1,7 @@
+
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/Workspace/go
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
+hash -d gomysrc="$GOPATH/src/rakuten"
+hash -d gorsrc="$GOPATH/src/github.com/parnurzeal"
