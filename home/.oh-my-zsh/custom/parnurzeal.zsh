e275db96c3a93c6cd0becd196ca1c2139e5cb33a
parnurzeal <parnurzeal@gmail.com>
2014-11-27 13:30:07 +0900

    Fixed wrong path to go workspace

--- home/.oh-my-zsh/custom/parnurzeal.zsh
+++ home/.oh-my-zsh/custom/parnurzeal.zsh
@@ -7,5 +7,5 @@ export GOROOT=/usr/local/go
 export GOPATH=$HOME/Workspace/go
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 
-hash -d gomysrc="$GOPATH/src/rakuten"
-hash -d gorsrc="$GOPATH/src/github.com/parnurzeal"
+hash -d gorsrc="$GOPATH/src/rakuten"
+hash -d gomysrc="$GOPATH/src/github.com/parnurzeal"
