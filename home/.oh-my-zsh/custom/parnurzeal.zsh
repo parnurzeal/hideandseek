f22e41e94dfb25f2d7df071c98d6b97ca83c8997
parnurzeal <parnurzeal@gmail.com>
2015-02-03 17:01:19 +0700

    Added ghq feature and peco ability

--- home/.oh-my-zsh/custom/parnurzeal.zsh
+++ home/.oh-my-zsh/custom/parnurzeal.zsh
@@ -4,12 +4,13 @@ export PATH="$PATH:/Users/teerapol.watanavekin/.usefulscript"
 alias subln="/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl -n"
 # Golang
 export GOROOT=/usr/local/go
-export GOPATH=$HOME/Workspace/go
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+export GOPATH=$HOME/PlayGround/ghq
+export GOBIN=$GOPATH/gobin
+export PATH=$PATH:$GOROOT/bin:$GOPATH/gobin
 
 hash -d gorsrc="$GOPATH/src/rakuten"
 hash -d gomysrc="$GOPATH/src/github.com/parnurzeal"
-hash -d interview="/Users/ParNurZeal/Playground/interview"
+hash -d interview="/Users/ParNurZeal/PlayGround/interview"
 
 # stop caching for completion in zsh
 zstyle ":completion:*:commands" rehash 1
