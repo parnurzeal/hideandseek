db3977e7d315afda9c436fe673a24d94f02fc187
parnurzeal <parnurzeal@gmail.com>
2016-11-24 20:32:10 +0800

    Add git editor

--- home/.oh-my-zsh/custom/plugins/my-git-config/my-git-config.plugin.zsh
+++ home/.oh-my-zsh/custom/plugins/my-git-config/my-git-config.plugin.zsh
@@ -6,3 +6,16 @@ git config --global user.email "${GIT_EMAIL}"
 # Set ghq path
 git config --global ghq.root "${HOME}/PlayGround/ghq"
 
+# Set core editor to vim
+git config --global core.editor $(which vim)
+
+# Set git alias
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global alias.cb "checkout -b"
+git config --global alias.cm commit
+git config --global alias.cma "commit -a"
+git config --global alias.cmam "commit --amend"
+git config --global alias.cmama "commit --amend -a"
+git config --global alias.w whatchanged
+git config --global alias.b branch
